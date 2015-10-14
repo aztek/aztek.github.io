b02d4d4fb9cb548a5571f9a6f2fa5aa64c23ae06
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2015-10-14 14:45:23 +0200

    Add all the materials to the experiments on FOOL page

--- fool-experiments/run-isabelle-experiments.sh
+++ fool-experiments/run-isabelle-experiments.sh
@@ -10,30 +10,31 @@ TIMEOUT=gtimeout
 # GNU date
 DATE=gdate
 
-for f in `cat isabelle-problems.txt`
+for PROBLEM in `cat isabelle-problems/problems.txt`
 do
-  echo -n $f
+  SMT_FILE="isabelle-problems/${PROBLEM}.smt_in"
+  TFF_FILE="isabelle-problems/${PROBLEM}.smt_in.p"
+
+  echo -n "${PROBLEM}"
   echo -en '\t'
 
   for VAMPIRE_MODE in "casc_sat" "casc"
   do
     VAMPIRE_TIME="$(${DATE} +%s%6N)"
-    VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT}s --fool_paramodulation on "smt_nada/$f.p"`
+    VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT}s --fool_paramodulation on "${TFF_FILE}"`
     VAMPIRE_TIME="$(($(${DATE} +%s%6N)-VAMPIRE_TIME))"
     echo -n "${VAMPIRE_MODE}"
     echo -en '\t'
     echo -n "${VAMPIRE_OUTPUT}" | sed -n 's/.*SZS status \([^ ]*\).*/\1/p' | tr -d '\n'
     echo -en '\t'
     echo -n "${VAMPIRE_TIME}"
-    echo -en '\t'
-    echo -n "${VAMPIRE_OUTPUT}" | sed -n 's/.*Generated clauses: \([^ ]*\).*/\1/p' | tail -1 | tr -d '\n'
     echo -en '\t'
   done
 
   for VAMPIRE_MODE in "casc_sat" "casc"
   do
     VAMPIRE_TIME="$(${DATE} +%s%6N)"
-    VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT}s --fool_paramodulation off --fool_ordering off "smt_nada/$f.p"`
+    VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT}s --fool_paramodulation off --fool_ordering off "${TFF_FILE}"`
     VAMPIRE_TIME="$(($(${DATE} +%s%6N)-VAMPIRE_TIME))"
     echo -n "${VAMPIRE_MODE}"
     echo -en '\t'
@@ -41,20 +42,18 @@ do
     echo -en '\t'
     echo -n "${VAMPIRE_TIME}"
     echo -en '\t'
-    echo -n "${VAMPIRE_OUTPUT}" | sed -n 's/.*Generated clauses: \([^ ]*\).*/\1/p' | tail -1 | tr -d '\n'
-    echo -en '\t'
   done
 
   CVC4_TIME="$(${DATE} +%s%6N)"
   # have to use gtimeout because CVC4 ignores the --tlimit option sometimes
-  CVC4_OUTPUT=`${TIMEOUT} 61s ${CVC4} --lang smt --output-lang tptp --tlimit=${TIME_LIMIT}000 smt_nada/${f}`
+  CVC4_OUTPUT=`${TIMEOUT} 61s ${CVC4} --lang smt --output-lang tptp --tlimit=${TIME_LIMIT}000 "${SMT_FILE}"`
   CVC4_TIME="$(($(${DATE} +%s%6N)-CVC4_TIME))"
   echo -n "${CVC4_OUTPUT}" | sed -n 's/.*SZS status \([^* ]*\).*/\1/p' | tr -d '\n'
   echo -en '\t'
   echo -en "${CVC4_TIME}"
 
   Z3_TIME="$(${DATE} +%s%6N)"
-  Z3_OUTPUT=`${Z3} -smt2 -T:${TIME_LIMIT} smt_nada/${f} 2>&-`
+  Z3_OUTPUT=`${Z3} -smt2 -T:${TIME_LIMIT} "${SMT_FILE}" 2>&-`
   Z3_TIME="$(($(${DATE} +%s%6N)-Z3_TIME))"
   echo -n "${Z3_OUTPUT}" | fgrep -v error | tr -d '\n'
   echo -en '\t'
--- fool-experiments/run-tptp-experiments.sh
+++ fool-experiments/run-tptp-experiments.sh
@@ -1,16 +1,19 @@
 TIME_LIMIT=60
 
 VAMPIRE=./vampire_with_fool
-SATALLAX=/Users/evgenyk/Downloads/satallax-2.8/bin/satallax.opt
-LEO=/Users/evgenyk/Downloads/leo2/bin/leo
-ISABELLE=/Applications/Isabelle2015.app/Isabelle/bin/isabelle
+SATALLAX=satallax.opt
+LEO=leo
+ISABELLE=isabelle
 
 # GNU date
 DATE=gdate
 
-for f in `ls thf-fool-experiments`
+for FILE in `ls tptp-problems/thf`
 do
-  SAT="$(cat thf-experiments/$f | fgrep -e "Status   : Satisfiable" -e "Status   : CounterSatisfiable" | wc -l | tr -d '\n')"
+  THF_FILE="tptp-problems/thf/${FILE}"
+  TFF_FILE="tptp-problems/fool-tff/${FILE}"
+
+  SAT="$(cat ${THF_FILE} | fgrep -e "Status   : Satisfiable" -e "Status   : CounterSatisfiable" | wc -l | tr -d '\n')"
   # a better way would be to read SPC, but it's not marked correctly for some problems
   if [ ${SAT} == "1" ]; then
     VAMPIRE_MODE=casc_sat
@@ -18,11 +21,11 @@ do
     VAMPIRE_MODE=casc
   fi
 
-  echo -n $f
+  echo -n "${FILE}"
   echo -en '\t'
 
   VAMPIRE_TIME="$(${DATE} +%s%6N)"
-  VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT} --fool_paramodulation on "thf-fool-experiments/$f"`
+  VAMPIRE_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT} --fool_paramodulation on "${TFF_FILE}"`
   VAMPIRE_TIME="$(($(${DATE} +%s%6N)-VAMPIRE_TIME))"
   echo -n "${VAMPIRE_MODE}"
   echo -en '\t'
@@ -30,53 +33,37 @@ do
   echo -en '\t'
   echo -n "${VAMPIRE_TIME}"
   echo -en '\t'
-  echo -n "${VAMPIRE_OUTPUT}" | sed -n 's/.*Generated clauses: \([^ ]*\).*/\1/p' | tail -1 | tr -d '\n'
-
-  echo -en '\t'
 
   VAMPIRE2_TIME="$(${DATE} +%s%6N)"
-  VAMPIRE2_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT} --fool_ordering off --fool_paramodulation off "thf-fool-experiments/$f"`
+  VAMPIRE2_OUTPUT=`${VAMPIRE} --mode ${VAMPIRE_MODE} -m 20000 -t ${TIME_LIMIT} --fool_ordering off --fool_paramodulation off "${TFF_FILE}"`
   VAMPIRE2_TIME="$(($(${DATE} +%s%6N)-VAMPIRE2_TIME))"
   echo -n "${VAMPIRE2_OUTPUT}" | sed -n 's/.*SZS status \([^ ]*\).*/\1/p' | tr -d '\n'
   echo -en '\t'
   echo -n "${VAMPIRE2_TIME}"
   echo -en '\t'
-  echo -n "${VAMPIRE2_OUTPUT}" | sed -n 's/.*Generated clauses: \([^ ]*\).*/\1/p' | tail -1 | tr -d '\n'
-
-  echo -en '\t'
 
   SATALLAX_TIME="$(gdate +%s%6N)"
-  SATALLAX_OUTPUT=`${SATALLAX} -verb 5 -t ${TIME_LIMIT} "thf-experiments/$f"`
+  SATALLAX_OUTPUT=`${SATALLAX} -verb 5 -t ${TIME_LIMIT} "${THF_FILE}"`
   SATALLAX_TIME="$(($(gdate +%s%6N)-SATALLAX_TIME))"
   echo -n "${SATALLAX_OUTPUT}" | sed -n 's/.*SZS status \([^* ]*\).*/\1/p' | tr -d '\n'
   echo -en '\t'
-  echo -en "${SATALLAX_TIME}" # elapsed time
-  echo -en '\t'
-  # Number of generated clauses: a sum for each subgoal?
-  echo -n "${SATALLAX_OUTPUT}" | sed -n 's/.*Proof found for a goal\.\.\.\([^* ]*\) clauses.*/\1/p' | awk '{s+=$1} END  {print s}' | tr -d '\n'
-
+  echo -en "${SATALLAX_TIME}"
   echo -en '\t'
 
   LEO_TIME="$(gdate +%s%6N)"
-  LEO_OUTPUT=`${LEO} --timeout ${TIME_LIMIT} --proofoutput 1 "thf-experiments/$f"`
+  LEO_OUTPUT=`${LEO} --timeout ${TIME_LIMIT} --proofoutput 1 "${THF_FILE}"`
   LEO_TIME="$(($(gdate +%s%6N)-LEO_TIME))"
-  # wtf? status is output twice
   echo -n "${LEO_OUTPUT}" | sed -n 's/.*SZS status \([^* ]*\).*/\1/p' | head -n 1 | tr -d '\n'
   echo -en '\t'
-  echo -en "${LEO_TIME}" # elapsed time
-  #echo -en '\t'
-  #no slauses information
+  echo -en "${LEO_TIME}"
   echo -en '\t'
 
   ISABELLE_TIME="$(gdate +%s%6N)"
-  ISABELLE_OUTPUT=`${ISABELLE} tptp_isabelle ${TIME_LIMIT} "thf-experiments/$f"`
+  ISABELLE_OUTPUT=`${ISABELLE} tptp_isabelle ${TIME_LIMIT} "${THF_FILE}"`
   ISABELLE_TIME="$(($(gdate +%s%6N)-ISABELLE_TIME))"
-  # wtf? status is output twice
   echo -n "${ISABELLE_OUTPUT}" | sed -n 's/.*SZS status \([^* ]*\).*/\1/p' | head -n 1 | tr -d '\n'
   echo -en '\t'
-  echo -en "${ISABELLE_TIME}" # elapsed time
-  #echo -en '\t'
-  #no slauses information
+  echo -en "${ISABELLE_TIME}"
   echo -en '\t'
 
   echo
