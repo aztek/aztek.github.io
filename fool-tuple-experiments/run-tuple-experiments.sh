a6d9e298efa39d269bbe202bac06acee1877d193
aztek <evgeny.kotelnikov@gmail.com>
2016-06-01 14:18:11 +0200

    Add supplementary material for the FM'16 submission

---
+++ fool-tuple-experiments/run-tuple-experiments.sh
@@ -0,0 +1,22 @@
+VAMPIRE="./vampire --newcnf on -t 60s"
+
+for benchmark in count_two count_two_flag count_three two_arrays three_arrays
+do
+	echo -en ${benchmark} '\t'
+	for unrolls in 2 3 4 5
+	do
+		FILE="${benchmark}/${benchmark}${unrolls}.p"
+		VAMPIRE_TIME="$(gdate +%s%6N)"
+		VAMPIRE_OUTPUT=`${VAMPIRE} ${FILE}`
+		VAMPIRE_STATUS=$?
+		VAMPIRE_TIME="$(($(gdate +%s%6N)-VAMPIRE_TIME))"
+		if [ ${VAMPIRE_STATUS} != 0 ]
+		then
+			echo -n '-'
+		else
+			echo -n `bc -l <<< "scale=3; ${VAMPIRE_TIME}/1000000"`
+		fi
+		echo -en '\t'
+	done
+	echo
+done
